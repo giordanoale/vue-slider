669e1d2031b2141fcd1d52669284d588216f8b3c
giordanoale <a.giordano89@gmail.com>
2021-03-25 18:00:01 +0100

    Aggiunto nome della foto visualizzata

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ var app = new Vue (
         el: "#root",
         data: {
             images: [
-                "https://files.salsacdn.com//article/7086_Positano/imagemain/Dollarphotoclub-75249551-1770x1152_z_0_63_514.20181107161534.jpg",
-                "https://www.costieraamalfitana.com/wp-content/uploads/2016/01/amalfi-1.jpg",
-                "https://images.placesonline.com/photos/424011211180417_Cetara_680552194.jpg",
-                "https://static.fanpage.it/wp-content/uploads/sites/4/2018/06/ravello-villa-rufolo-panoramio.jpg",
+                {
+                    img: "https://files.salsacdn.com//article/7086_Positano/imagemain/Dollarphotoclub-75249551-1770x1152_z_0_63_514.20181107161534.jpg",
+                    nome: "Positano",
+                },
+                {
+                    img: "https://www.costieraamalfitana.com/wp-content/uploads/2016/01/amalfi-1.jpg",
+                    nome: "Amalfi",
+                },
+                {
+                    img: "https://images.placesonline.com/photos/424011211180417_Cetara_680552194.jpg",
+                    nome: "Cetara",
+                },
+                {
+                    img: "https://static.fanpage.it/wp-content/uploads/sites/4/2018/06/ravello-villa-rufolo-panoramio.jpg",
+                    nome: "Ravello",
+                },
             ],
             imageIndex: 0,
         },
